1824c5748c0ed65820e1d4daa924fe9b771b2f9a
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-12 21:19:00 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,9 +10,11 @@ class Patient
 
   def add_appointment(appointment)
     @appointments << appointment
+    appointment.patient = self
+    # This method should also tell the appointment that it belongs to this patient.
   end
-
-
+    
+    
 
 
 
